ac3b514b15fc3fd50b721afd3c60f0283c7e4d0b
hannahgreen1 <hannahgreen94@icloud.com>
2018-04-20 15:15:46 +0100

    11 tests passing in room

--- rooms.rb
+++ rooms.rb
@@ -1,27 +1,28 @@
 class Room
 
-attr_reader :room, :guests, :playlist
+attr_reader :name, :guests, :playlist, :capacity
 
-  def initialize(room)
-    @room = room
-    @guest = []
+  def initialize(name, capacity)
+    @name = name
+    @guests = []
     @playlist = []
+    @capacity = capacity
   end
 
-def count_guests
-  @guest.size()
-end
+  def count_guests
+    @guests.size()
+  end
 
-def count_playlist
-  @playlist.size()
-end
+  def count_playlist
+    @playlist.size()
+  end
 
   def check_in_guest_to_room(guest)
-   @guest.push(guest)
+   @guests.push(guest) unless is_full?
   end
 
   def check_out_guest_to_room(guest)
-   @guest.pop
+   @guests.pop
   end
 
   def add_song_to_playlist(song)
@@ -31,4 +32,10 @@ end
   def remove_all_songs_from_playlist(song)
    @playlist.delete(song)
   end
+
+  def is_full?
+    @guests.count() == capacity
+  end
+
+
 end
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -9,18 +9,17 @@ require_relative('../guests')
 class RoomTest < MiniTest::Test
 
   def setup
-    @room1 = Room.new("Red Room")
-    @room2 = Room.new("Blue Room")
-    @room3 = Room.new("Yellow Room")
+    @room1 = Room.new("Blue Room", 3)
 
     @guest1 = Guests.new("Hannah")
     @guest2 = Guests.new("Davina")
     @guest3 = Guests.new("Rachel")
+    @guest4 = Guests.new("Colin")
   end
 
   def test_room_name
     answer = "Blue Room"
-    result =  @room2.room
+    result =  @room1.name
     assert_equal(answer,result)
   end
 
@@ -57,5 +56,36 @@ class RoomTest < MiniTest::Test
     @room1.add_song_to_playlist(@song3)
     @room1.remove_all_songs_from_playlist(@song3)
     assert_equal(0, @room1.count_playlist)
+  end
+
+  def test_room_has_capacity
+    assert_equal(3, @room1.capacity)
+  end
+
+  def test_room_is_full__false
+    @room1.check_in_guest_to_room(@guest1)
+    @room1.check_in_guest_to_room(@guest2)
+    assert_equal(false, @room1.is_full?)
+  end
+
+  def test_room_is_full__true
+    @room1.check_in_guest_to_room(@guest1)
+    @room1.check_in_guest_to_room(@guest2)
+    @room1.check_in_guest_to_room(@guest3)
+    assert_equal(true, @room1.is_full?)
+  end
+
+  def test_refuse_guest
+    @room1.check_in_guest_to_room(@guest1)
+    @room1.check_in_guest_to_room(@guest2)
+    @room1.check_in_guest_to_room(@guest3)
+    # ACT
+    @room1.check_in_guest_to_room(@guest4)
+    expected_guests = [
+      @guest1,
+      @guest2,
+      @guest3
+    ]
+    assert_equal(expected_guests, @room1.guests)
   end
 end
